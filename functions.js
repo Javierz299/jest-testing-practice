f05eb5bad9ce6bf6b768905fd8a6ec2586a63be4
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-10-08 12:34:26 -0400

    4 tests

--- functions.js
+++ functions.js
@@ -1,8 +1,20 @@
 const functions = {
-add: (num1,num2) => {
-        console.log(num1)
-        return num1 + num2;
-    }
+    add: (num1,num2) => {
+            return num1 + num2;
+        },
+    isNull: () => {
+        return null;//
+    },
+    checkValue: (x) => {
+        return x;
+    },
+    creatUser: () => {
+        const user = {
+            firstName: 'Javi',
+        }
+        user['lastName'] = 'Zapien'
+        return user;
+    },
 }
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -1,6 +1,39 @@
 const functions = require('./functions')
 
 
-it('Adds 2 + 2 to equal 4', () => {
+test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2,2)).toBe(4)
-})
+});
+
+// test('ad 2 + 2 not equal to 5', () => {
+//     expect(functions.add(2,3)).toBe(4)
+// })
+
+//CHECK for truthy & falsy values
+//toBeNull matches only null
+//toBeUndefined matches only undefined
+//toBeDefined is the opposite of toBeUndefined
+//toBeTruthy matches anything that an if statement treats as true
+//toBeFalsy matches anything that an if statment treats as false
+
+//toBe more for primitive types
+
+test('should be null', () => {
+    expect(functions.isNull()).toBeNull()
+});
+ 
+test('should be falsy', () => {
+    //null - 0 - undefined return falsy
+    expect(functions.checkValue(undefined)).toBeFalsy()
+});
+
+//toEqual - objects and arrays
+//If it should pass with deep equality, 
+//replace with "toStrictEqual"
+test('user should be Javi Zapien Object', () => {
+    //null - 0 - undefined return falsy
+    expect(functions.creatUser()).toEqual({
+        firstName: 'Javi',
+        lastName: 'Zapien'
+    });
+});
